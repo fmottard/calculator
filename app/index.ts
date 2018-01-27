eaeb5175a36e68f4f45534b16033dd2693ae7e43
fmottard <francoismottard30@gmail.com>
2018-01-27 13:43:06 -0600

    Initial

---
+++ app/index.ts
@@ -0,0 +1,69 @@
+var result:number = +'10';
+var left='0';
+var right='0';
+var operateur = '';
+function add(valeur:string){
+    if(operateur.length > 0){
+        right = right + valeur;
+        document.getElementById('texte').textContent = left + operateur + right;            
+        
+    }else{
+        left = left + valeur;
+        document.getElementById('texte').textContent = left;                    
+    }
+}
+
+
+function reset(){
+    left = '0';
+    right = '0';
+    operateur = '';
+    document.getElementById('texte').textContent = '--';        
+}
+
+function operation(symbole:string){
+    operateur = symbole;
+    document.getElementById('texte').textContent = left + operateur + right;            
+}
+
+
+function additionne(){
+    operation('+');
+    document.getElementById('texte').textContent = left + operateur + right;            
+}
+
+function soustrait(){
+    operation('-');
+    document.getElementById('texte').textContent = left + operateur + right;            
+}
+
+function multiplie(){
+    operation('*');
+    document.getElementById('texte').textContent = left + operateur + right;            
+}
+function divise(){
+    operation('/');
+    document.getElementById('texte').textContent = left + operateur + right;            
+}
+
+
+
+function egal(){
+    if(operateur === '+'){
+        var total = Number(left) + Number(right);
+        document.getElementById('texte').textContent = total.toString();
+    }
+    if(operateur === '-'){
+        var total = Number(left) - Number(right);
+        document.getElementById('texte').textContent = total.toString();
+    }
+    if(operateur === '*'){
+        var total = Number(left) * Number(right);
+        document.getElementById('texte').textContent = total.toString();
+    }
+    if(operateur === '/'){
+        var total = Number(left) / Number(right);
+        document.getElementById('texte').textContent = total.toString();
+    }
+                    
+}
